6069e276218e2f7ac93c2cf023037de54dd570c1
Andre Gaio <andregaio23@gmail.com>
2023-09-19 08:30:32 +0100

    docs: cleaned up documentation

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
 dcgan
 
 A PyTorch implementation of [Unsupervised Representation Learning With Deep Convolutional Generative Adversarial Networks](https://arxiv.org/pdf/1511.06434.pdf)
+
+
+<img src="assets/logo.png">
+
+ 
+### Installation
+```
+git clone https://github.com/andregaio/dcgan.git
+cd dcgan
+conda create -n dcgan python=3.8
+conda activate dcgan
+pip install -r requirements.txt
+```
+
+
+### Models
+ - DCGAN
+
+### Dataset
+[Celeb-A Faces](http://mmlab.ie.cuhk.edu.hk/projects/CelebA.html) has been used for training. Download and unzip 'img_align_celeba.zip' from [Google Drive](https://drive.google.com/drive/folders/0B7EVK8r0v71pTUZsaXdaSnZBZzg?resourcekey=0-rJlzl934LzC-Xp28GeIBzQ) into the 'dataset' folder
+
+### Training
+```
+python dcgan/train.py
+```
+
+### Inference
+```
+python dcgan/infer.py --weights weights/last_weights.pt --out test/image.png
+```
+
+### [Results](https://wandb.ai/andregaio/vgg)
+<div align="center">
+
+
+<img src="assets/chart.png">
+
+
+</div>
+
+### Notes
+ - This is a refactored implementation of https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html
--- dcgan/train.py
+++ dcgan/train.py
@@ -5,6 +5,8 @@ import torch.nn.parallel
 import torch.optim as optim
 import torch.utils.data
 import torchvision.datasets as dset
+import wandb
+import argparse
 from model.dcgan import DCGAN
 from data import celeb_data_transforms
 
@@ -15,77 +17,104 @@ torch.manual_seed(manualSeed)
 torch.use_deterministic_algorithms(True)
 
 
-dataroot = "/home/andre/repos/dcgan/dataset/"
-batch_size = 128
-nz = 100
-num_epochs = 5
-lr = 0.0002
-beta1 = 0.5
+def train(args):
 
+    if args.wandb:
+        run = wandb.init(
+            project='dcgan',            
+            config={
+                'dcgan': 'dcgan',
+                'dataset': args.dataset,
+                'epochs': args.epochs,
+                'batch' : args.batch,
+                'loss' : 'sgd',
+                'learning_rate': args.learning_rate,
+                'momentum' : args.momentum,
+                'weight_decay' : args.weight_decay,
+            })
 
-device = torch.device("cuda:0")
+    dataroot = "/home/andre/repos/dcgan/dataset/"
+    batch_size = 128
+    num_epochs = 5
+    lr = 0.0002
+    beta1 = 0.5
 
+    device = torch.device("cuda:0")
 
-dataset = dset.ImageFolder(root=dataroot, transform=celeb_data_transforms)
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+    dataset = dset.ImageFolder(root=dataroot, transform=celeb_data_transforms)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=2)
 
+    model = DCGAN()
+    model.initialise()
+    model.generator = model.generator.to(device)
+    model.discriminator = model.discriminator.to(device)
 
-model = DCGAN()
-model.initialise()
-model.generator = model.generator.to(device)
-model.discriminator = model.discriminator.to(device)
+    criterion = nn.BCELoss()
 
+    # Establish convention for real and fake labels during training
+    real_label = 1.
+    fake_label = 0.
 
-criterion = nn.BCELoss()
+    optimizer_discriminator = optim.Adam(model.discriminator.parameters(), lr=lr, betas=(beta1, 0.999))
+    optimizer_generator = optim.Adam(model.generator.parameters(), lr=lr, betas=(beta1, 0.999))
 
-
-# Establish convention for real and fake labels during training
-real_label = 1.
-fake_label = 0.
-
-
-optimizer_discriminator = optim.Adam(model.discriminator.parameters(), lr=lr, betas=(beta1, 0.999))
-optimizer_generator = optim.Adam(model.generator.parameters(), lr=lr, betas=(beta1, 0.999))
-
-
-for epoch in range(num_epochs):
-    
-    for i, data in enumerate(dataloader, 0):
-
-        # Update Discriminator
-        # Real batch
-        model.discriminator.zero_grad()
-        x_real = data[0].to(device)
-        y = torch.full((x_real.size(0),), real_label).to(device)
-        y_hat = model.discriminator(x_real).view(-1)
-        loss_discriminator_real = criterion(y_hat, y)
-        loss_discriminator_real.backward()
-
-        # Fake batch
-        x_fake = model.generator.generate_tensor(x_real.size(0))
-        y.fill_(fake_label)
-        y_hat = model.discriminator(x_fake.detach()).view(-1)
-        loss_discriminator_fake = criterion(y_hat, y)
-        loss_discriminator_fake.backward()
-        loss_discriminator = loss_discriminator_real + loss_discriminator_fake
-        optimizer_discriminator.step()
-
-        # Update Generator
-        model.generator.zero_grad()
-        y.fill_(real_label)
-        y_hat = model.discriminator(x_fake).view(-1)
-        loss_generator = criterion(y_hat, y)
-        loss_generator.backward()
-        optimizer_generator.step()
+    for epoch in range(num_epochs):
         
-        # Output training stats
-        if i % 50 == 0:
-            print(f'[{epoch + 1}/{num_epochs}][{i}/{len(dataloader)}]\tLoss_D: {loss_discriminator.item()}\tLoss_G: {loss_generator.item()}')
+        for i, data in enumerate(dataloader, 0):
 
-        model.generator.generate_image()
+            # Update Discriminator
+            # Real batch
+            model.discriminator.zero_grad()
+            x_real = data[0].to(device)
+            y = torch.full((x_real.size(0),), real_label).to(device)
+            y_hat = model.discriminator(x_real).view(-1)
+            loss_discriminator_real = criterion(y_hat, y)
+            loss_discriminator_real.backward()
 
-    model.save_checkpoint('weights/last_weights.pt')
+            # Fake batch
+            x_fake = model.generator.generate_tensor(x_real.size(0))
+            y.fill_(fake_label)
+            y_hat = model.discriminator(x_fake.detach()).view(-1)
+            loss_discriminator_fake = criterion(y_hat, y)
+            loss_discriminator_fake.backward()
+            loss_discriminator = loss_discriminator_real + loss_discriminator_fake
+            optimizer_discriminator.step()
 
-    
+            # Update Generator
+            model.generator.zero_grad()
+            y.fill_(real_label)
+            y_hat = model.discriminator(x_fake).view(-1)
+            loss_generator = criterion(y_hat, y)
+            loss_generator.backward()
+            optimizer_generator.step()
+            
+            # Output training stats
+            if i % 50 == 0:
+                print(f'[{epoch + 1}/{num_epochs}][{i}/{len(dataloader)}]\tLoss_D: {loss_discriminator.item()}\tLoss_G: {loss_generator.item()}')
 
-        
+            if args.wandb:
+                wandb.log({'loss_discriminator_real': loss_discriminator_real,
+                            'loss_discriminator_fake': loss_discriminator_fake,
+                            'loss_generator': loss_generator,
+                            'epoch': epoch + 1,
+                        })
+            
+            model.generator.generate_image()
+
+        model.save_checkpoint('weights/last_weights.pt')
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser("Argument parser")
+    parser.add_argument("--model", type = str, default = 'vgg_A')
+    parser.add_argument("--dataset", type = str, default = 'cifar10')
+    parser.add_argument("--batch", type = int, default = 128)
+    parser.add_argument("--epochs", type = int, default = 74)
+    parser.add_argument("--learning_rate", type = float, default = 10e-3)
+    parser.add_argument("--weight_decay", type = float, default = 5 * 10e-4)
+    parser.add_argument("--momentum", type = str, default = 0.9)
+    parser.add_argument("--wandb", action="store_true", default = False)
+    args = parser.parse_args()
+
+    train(args)
